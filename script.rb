bee7a168eab3930cc3ea23d80ff46179c0aba3d8
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-09-09 20:44:10 -0700

    Implement caesar_cipher method

--- script.rb
+++ script.rb
@@ -1,5 +1,26 @@
+ASCII_UP_START = 65
+ASCII_UP_LIMIT = 90
+ASCII_DOWN_START = 97
+ASCII_DOWN_LIMIT = 121
+WRAP = 26
 
-
-def caesar_cypher (string, shift_factor)
- 
+def caesar_cipher (string, shift_factor)
+  cypher = string.chars.map do |c|
+    c = c.ord
+    if c >= ASCII_UP_START && c <= ASCII_UP_LIMIT
+      c += shift_factor
+      c -= WRAP if c > ASCII_UP_LIMIT
+      c += WRAP if c < ASCII_UP_START
+    elsif c >= ASCII_DOWN_START && c <= ASCII_DOWN_LIMIT
+      c += shift_factor
+      c -= WRAP if c > ASCII_DOWN_LIMIT
+      c += WRAP if c < ASCII_DOWN_START
+    end
+    c.chr
+  end
+  puts cypher.join
 end
+
+
+caesar_cipher("Hello!", 3)
+caesar_cipher("What a string!", 5)
